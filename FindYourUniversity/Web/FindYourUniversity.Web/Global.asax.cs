67a1067ed91887e97da459a8636c302fb4fec93b
georgikostadinov <georgikostadinov@yahoo.com>
2014-11-09 18:57:41 +0200

    Configured AutoMapper

--- FindYourUniversity/Web/FindYourUniversity.Web/Global.asax.cs
+++ FindYourUniversity/Web/FindYourUniversity.Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using FindYourUniversity.Web.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,9 @@ namespace FindYourUniversity.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var automapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            automapperConfig.Execute();
         }
     }
 }
