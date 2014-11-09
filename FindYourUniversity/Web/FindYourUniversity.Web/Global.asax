6e3fad1a00c692c3e9b582829c9ca34ca659b70b
georgikostadinov <georgikostadinov@yahoo.com>
2014-11-09 14:09:12 +0200

    Added solution with empty projects and libraries

---
+++ FindYourUniversity/Web/FindYourUniversity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FindYourUniversity.Web.MvcApplication" Language="C#" %>
---
+++ FindYourUniversity/Web/FindYourUniversity.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindYourUniversity.Web.Startup))]
+namespace FindYourUniversity.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
