75e9224eb870fc32abfeb6894e12e2afdb8703d7
georgikostadinov <georgikostadinov@yahoo.com>
2014-11-13 19:18:57 +0200

    Added ProgrammesController with dropdown editing functionality

---
+++ FindYourUniversity/Web/FindYourUniversity.Web/Areas/UniversityArea/Controllers/ProgrammesController.cs
@@ -0,0 +1,46 @@
+﻿using FindYourUniversity.Data;
+using FindYourUniversity.Web.Areas.UniversityArea.Controllers.Base;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper.QueryableExtensions;
+using FindYourUniversity.Web.Areas.UniversityArea.ViewModels;
+using Kendo.Mvc.Extensions;
+
+namespace FindYourUniversity.Web.Areas.UniversityArea.Controllers
+{
+    public class ProgrammesController : UniversityBaseController
+    {
+        public ProgrammesController(IFindYourUniversityData data)
+            : base(data)
+        { 
+        }
+
+        // GET: UniversityArea/Programmes
+        public ActionResult Index()
+        {
+            ViewData["degrees"] = this.Data.Degrees.All().ToArray();
+            var faculties = this.Data.Faculties.All().Select(f => new 
+            {
+                Id = f.Id,
+                Name = f.Name
+            });
+            ViewData["faculties"] = faculties.ToArray();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var programmes = this.Data.Programmes.All()
+                //.Where(p => p.Faculty.University.Id == this.CurrentUniversity.Id)
+                .Project().To<ProgrammeViewModel>();
+
+            return Json(programmes.ToDataSourceResult(request));
+        }
+    }
+}
---
+++ FindYourUniversity/Web/FindYourUniversity.Web/Areas/UniversityArea/ViewModels/ProgrammeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using FindYourUniversity.Data.Models;
+using FindYourUniversity.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindYourUniversity.Web.Areas.UniversityArea.ViewModels
+{
+    public class ProgrammeViewModel : IMapFrom<Programme>, IHaveCustomMappings
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Display(Name="Име")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [UIHint("GridForeignKey")]
+        [Display(Name = "Образователна степен")]
+        public int DegreeId { get; set; }
+
+        [UIHint("GridForeignKey")]
+        [Display(Name = "Факултет")]
+        public int FacultyId { get; set; }
+
+        public void CreateMappings(AutoMapper.IConfiguration configuration)
+        {
+            configuration.CreateMap<Programme, ProgrammeViewModel>().ReverseMap();
+        }
+    }
+}
