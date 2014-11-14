3e35c59abdafad2671cb332e7b724966a21a25e3
georgikostadinov <georgikostadinov@yahoo.com>
2014-11-14 14:45:24 +0200

    Finished the CRUD operations in the UniversityArea

--- FindYourUniversity/Web/FindYourUniversity.Web/Areas/UniversityArea/Controllers/CoursesController.cs
+++ FindYourUniversity/Web/FindYourUniversity.Web/Areas/UniversityArea/Controllers/CoursesController.cs
@@ -1,10 +1,16 @@
 ﻿using FindYourUniversity.Data;
 using FindYourUniversity.Web.Areas.UniversityArea.Controllers.Base;
+using FindYourUniversity.Web.Areas.UniversityArea.ViewModels;
+using Kendo.Mvc.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper.QueryableExtensions;
+using Kendo.Mvc.Extensions;
+using AutoMapper;
+using FindYourUniversity.Data.Models;
 
 namespace FindYourUniversity.Web.Areas.UniversityArea.Controllers
 {
@@ -16,12 +22,27 @@ namespace FindYourUniversity.Web.Areas.UniversityArea.Controllers
         // GET: UniversityArea/Courses
         public ActionResult Index()
         {
+            var programmes = this.Data.Programmes.All().Select(p => new 
+            {
+                Id = p.Id,
+                Name = p.Name
+            });
+
+            ViewData["programmes"] = programmes;
+
             return View();
         }
 
         public JsonResult GetCascadeDegrees()
         {
-            var degrees = this.Data.Degrees.All().Select(d => new { Id = d.Id, Name = d.Name });
+            var degrees = this.Data.Degrees
+                .All()
+                .Select(d => new 
+                {
+                    DegreeId = d.Id,
+                    DegreeName = d.Name
+                });
+
             return Json(degrees, JsonRequestBehavior.AllowGet);
         }
 
@@ -34,8 +55,8 @@ namespace FindYourUniversity.Web.Areas.UniversityArea.Controllers
                     .All()
                     .Select(f => new
                     {
-                        Id = f.Id,
-                        Name = f.Name
+                        FacultyId = f.Id,
+                        FacultyName = f.Name
                     });
 
                 return Json(faculties, JsonRequestBehavior.AllowGet);
@@ -54,14 +75,66 @@ namespace FindYourUniversity.Web.Areas.UniversityArea.Controllers
                     .Where(p => p.DegreeId == degrees && p.FacultyId == faculties)
                     .Select(p => new
                     {
-                        Id = p.Id,
-                        Name = p.Name
+                        ProgrammeId = p.Id,
+                        ProgrammeName = p.Name
                     });
 
                 return Json(programmes, JsonRequestBehavior.AllowGet);
             }
 
             return null;
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request, int ProgId)
+        {
+            var courses = this.Data.Courses.All().Where(c=>c.ProgrammeId == ProgId)
+                .Project().To<CourseViewModel>();
+
+            return Json(courses.ToDataSourceResult(request));
+        }
+
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, CourseViewModel model, int ProgId)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                model.ProgrammeId = ProgId;
+                var dbModel = Mapper.Map<CourseViewModel, Course>(model);
+                this.Data.Courses.Add(dbModel);
+                this.Data.SaveChanges();
+                model.Id = dbModel.Id;
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, CourseViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var course = this.Data.Courses.GetById(model.Id);
+                Mapper.Map<CourseViewModel, Course>(model, course);
+                this.Data.Courses.Update(course);
+                this.Data.SaveChanges();
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, [Bind(Include = "Id")] CourseViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var courseToDelete = this.Data.Courses.GetById(model.Id);
+                this.Data.Courses.Delete(courseToDelete);
+                this.Data.SaveChanges();
+            }
+
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState));
         }
     }
 }
---
+++ FindYourUniversity/Web/FindYourUniversity.Web/Areas/UniversityArea/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FindYourUniversity.Data.Models;
+using FindYourUniversity.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindYourUniversity.Web.Areas.UniversityArea.ViewModels
+{
+    public class CourseViewModel : IMapFrom<Course>, IHaveCustomMappings
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Semester { get; set; }
+        public int Credits { get; set; }
+        public int Hours { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public int ProgrammeId { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Course, CourseViewModel>().ReverseMap();
+        }
+    }
+}
